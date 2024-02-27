9e7aef8041603e07d70c11e0e7cd0656bbc49b5c
smit.groovy <smitchauhan@groovyweb.co>
2024-02-27 11:22:12 +0530

    Add : setup Redux

---
+++ app/Redux/Reducers/auth/reducer.js
@@ -0,0 +1,15 @@
+import types from './actions';
+
+const initialState = {
+  accessToken: '',
+};
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case types.SET_ACCESS_TOKEN:
+      return {
+        ...state,
+        accessToken: action.accessToken,
+      };
+  }
+}
