7b6662609d072a51e1b31b53bbc4f77ac44599dd
smit.groovy <smitchauhan@groovyweb.co>
2024-02-28 15:25:49 +0530

    Add : simple push notification using firebase

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text, View} from 'react-native';
 import StartingNav from './app/navigation';
 import {ThemeProvider} from '@react-navigation/native';
 import {Provider} from 'react-redux';
 import {persistor, store} from './app/Redux/store/configStore';
 import {PersistGate} from 'redux-persist/integration/react';
+import {firebase} from '@react-native-firebase/messaging';
 
 const App = () => {
+  useEffect(() => {
+    getDeviceToken();
+  }, []);
+
+  const getDeviceToken = async () => {
+    try {
+      await firebase.messaging().registerDeviceForRemoteMessages();
+      let token = await firebase.messaging().getToken();
+      console.log('token======>>>>>', token);
+    } catch (err) {
+      console.log('err======>>>>>', err);
+    }
+  };
+
   return (
     <View style={{flex: 1}}>
       <ThemeProvider>
